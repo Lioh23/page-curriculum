62a76e59d9da4e9573089b89665409a154f8fae4
Aurélio Silva de Jesus <aurelin95@gmail.com>
2022-01-01 12:53:57 -0300

    fixing responsive behavior

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const btnToggle = document.querySelector('.btn__toggle');
 const curriculum = document.querySelector('.curriculum');
+const sideLeft = document.querySelector('.curriculum__side-left');
 
 btnToggle.addEventListener('click', event => {
 
     event.preventDefault();
-    curriculum.classList.toggle('currculum--side-left-hidden');
+    sideLeft.classList.toggle('currculum__side-left--hidden');
+    // main.classList.toggle('main--width-100');
 });
+
+window.addEventListener('resize', event => {
+    if(window.innerWidth >= 1150) {
+        sideLeft.classList.remove('currculum__side-left--hidden');
+    };
+})
